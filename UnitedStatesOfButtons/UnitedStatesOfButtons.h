3eee0a960a4deed17c719cab2a91904366ee580d
Matt Ronan <mattRonanDev@gmail.com>
2022-02-09 14:49:28 -0500

    Update UnitedStatesOfButtons.h

--- UnitedStatesOfButtons/UnitedStatesOfButtons.h
+++ UnitedStatesOfButtons/UnitedStatesOfButtons.h
@@ -9,108 +9,108 @@
 
 class SevenStateButton
 {
-	public:
-		SevenStateButton(int pin,int pullup);
+  public:
+    SevenStateButton(int pin,int pullup);
     void update();
     int state();
-		int doubleClick();
-		int getPin();
+    int doubleClick();
+    int getPin();
     int stickyLowTime;
-		unsigned long longPressThreshold;
-		unsigned long retriggerWait;
-		unsigned long doubleClickThreshold;
-	
-	private:
+    unsigned long longPressThreshold;
+    unsigned long retriggerWait;
+    unsigned long doubleClickThreshold;
+  
+  private:
     int _pin;
-		int doubleClickState;
-		int buttonState;
-		unsigned long buttonPressedAt;
-		unsigned long buttonReleaseTracker;
+    int doubleClickState;
+    int buttonState;
+    unsigned long buttonPressedAt;
+    unsigned long buttonReleaseTracker;
 };
 
 class SixStateButton
 {
-	public:
-		SixStateButton(int pin,int pullup);
+  public:
+    SixStateButton(int pin,int pullup);
     void update();
     int state();
-		int doubleClick();
-		int getPin();
+    int doubleClick();
+    int getPin();
     int stickyLowTime;
-		unsigned long longPressThreshold;
-		unsigned long retriggerWait;
-		unsigned long doubleClickThreshold;
-	
-	private:
+    unsigned long longPressThreshold;
+    unsigned long retriggerWait;
+    unsigned long doubleClickThreshold;
+  
+  private:
     int _pin;
-		int doubleClickState;
-		int buttonState;
-		unsigned long buttonPressedAt;
-		unsigned long buttonReleaseTracker;
-	
+    int doubleClickState;
+    int buttonState;
+    unsigned long buttonPressedAt;
+    unsigned long buttonReleaseTracker;
+  
 };
 
 class FourStateButton
 {
-	public:
-		FourStateButton(int pin,int pullup);
+  public:
+    FourStateButton(int pin,int pullup);
     void update();
     int state();
-		int doubleClick();
-		int getPin();
+    int doubleClick();
+    int getPin();
     int stickyLowTime;
-		unsigned long longPressThreshold;
-		unsigned long retriggerWait;
-		unsigned long doubleClickThreshold;
-	
-	private:
+    unsigned long longPressThreshold;
+    unsigned long retriggerWait;
+    unsigned long doubleClickThreshold;
+  
+  private:
     int _pin;
-		int doubleClickState;
-		int buttonState;
-		unsigned long buttonPressedAt;
-		unsigned long buttonReleaseTracker;
-	
+    int doubleClickState;
+    int buttonState;
+    unsigned long buttonPressedAt;
+    unsigned long buttonReleaseTracker;
+  
 };
 
 class ThreeStateButton
 {
-	public:
-		ThreeStateButton(int pin,int pullup);
-		void update();
-		int state();
-		int doubleClick();
-		int getPin();
-		int stickyLowTime;
-		unsigned long retriggerWait;
-		unsigned long doubleClickThreshold;
-	
-	private:
+  public:
+    ThreeStateButton(int pin,int pullup);
+    void update();
+    int state();
+    int doubleClick();
+    int getPin();
+    int stickyLowTime;
+    unsigned long retriggerWait;
+    unsigned long doubleClickThreshold;
+  
+  private:
     int _pin;
-		int doubleClickState;
-		int buttonState;
-		unsigned long buttonPressedAt;
-		unsigned long buttonReleaseTracker;
-	
+    int doubleClickState;
+    int buttonState;
+    unsigned long buttonPressedAt;
+    unsigned long buttonReleaseTracker;
+  
 };
 
 class TwoStateButton
 {
-	public:
-		TwoStateButton(int pin,int pullup);
+  public:
+    TwoStateButton(int pin,int pullup);
     void update();
     int state();
-		int doubleClick();
-		int getPin();
+    int doubleClick();
+    int getPin();
     int stickyLowTime;
-		unsigned long retriggerWait;
-		unsigned long doubleClickThreshold;
-	
-	private:
+    unsigned long retriggerWait;
+    unsigned long doubleClickThreshold;
+  
+  private:
     int _pin;
-		int doubleClickState;
-		int buttonState;
-		unsigned long buttonPressedAt;
-		unsigned long buttonReleaseTracker;
-	
+    int doubleClickState;
+    int buttonState;
+    unsigned long buttonPressedAt;
+    unsigned long buttonReleaseTracker;
+  
 };
 #endif
